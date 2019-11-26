f6d3327b6e4f009d673d677ebc49aa128cd66cd2
Miranda Spoelstra <Miranda.Spoelstra@ing.com>
2019-11-26 12:04:15 +0100

    added more variables for efficiency

--- javascript/roman_numeral_convertor.js
+++ javascript/roman_numeral_convertor.js
@@ -6,16 +6,19 @@ All roman numerals answers should be provided in upper-case.
 
 function convertToRoman(num) {
     const romanNumerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-    const decimalValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const decimalVals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const valuesSize = decimalVals.length;
     let romanized = '';
 
-    for (let i = 0; i < decimalValues.length; i++) {
-        while (num >= decimalValues[i]) {
-            romanized += romanNumerals[i];
-            num -= decimalValues[i];
+    // note: if used or iterated over more than once, store something in a variable. Is more efficient
+    for (let i = 0; i < valuesSize; i++) {
+        const decimalVal = decimalVals[i];
+        const romanVal = romanNumerals[i];
+        while (num >= decimalVal) {
+            romanized += romanVal;
+            num -= decimalVal;
         }
     }
-    console.log(romanized);
     return romanized;
 }
 
